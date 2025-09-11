4caa643cc7695f852534eb3c891fd8a632f847af
lab-01-minhphuluong-ai <minhphu.luong@ontariotechu.net>
2025-09-11 17:27:51 -0400

    Lab complete, this time with comments!

--- py lab01.py
+++ py lab01.py
@@ -2,7 +2,9 @@ def main():
     cost_per_item = 19.99
     quantity = 5 
 
-    # YOUR CODE FOR PART 1 GOES HERE  
+    # YOUR CODE FOR PART 1 GOES HERE
+    cost_per_item = 19.99
+    quantity = 5  
     subtotal_cost = cost_per_item*quantity 
     tax = subtotal_cost*0.13
     total_cost = subtotal_cost + tax
